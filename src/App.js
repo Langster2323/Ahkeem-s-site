53cd11964189783fe24e76156bb0a37b3c215535
Langster2323 <langahkeem@gmail.com>
2020-11-18 17:05:14 -0600

    updated what I do section

--- src/App.js
+++ src/App.js
@@ -30,26 +30,32 @@ function App() {
             <h2 className="section__title section__title--services">What I do</h2>
             <div className="services">
                 <div className="service">
-                    <h3>Development</h3>
-                    <p>For nearly three years I've been a front end developer here in the Austin, 
-                      Texas. Building Single Page Applications out of ReactJS and AngularJS, 
-                      I have experience working within the education, civil engineering, 
-                      public sector and health care blockchain industries. </p>
+                    <h3>Data Engineer</h3>
+                    <p>My data engineer experience, isn't as extensive as my 
+                        Full Stack Experience but I really do enjoy data engineering.
+                        At the moment I use PowerBI to visualize my data and I use the 
+                        BI connector to connect PowerBI with my data source MongoDB Atlas. I've 
+                        used Azure Data Factory and ETL pipeline to ingest data into a Azure 
+                        Data Lake Generation two. I'm currently training for data engineering 
+                        certifications, I have plans to learn Machine Learning using frameworks 
+                        like Tensorflow 2.0 and I have learning Python and SQL on my list.</p>
                 </div> 
                 
                 <div className="service">
                     <h3>Skills</h3>
-                    <p>ReactJS | Redux | NodeJS | ExpressJS | AngularJS | JavaScript |  TypeScript | Axios | RESTful API | ES6 | HTML5 | CSS3 | Google Analytics |  Agile | Scrum | Flexbox | Kendo UI | TailwindCSS | Docker | Git | GitHub | Gitlab | Linux | Ruby on Rails | MVC | Webpack| Bootstrap.</p>
+                            <p>ReactJS | Hooks | Context API | GraphQL | PowerBI | Azure Data Factory | Redux | NodeJS | ExpressJS | MongoDB | JavaScript | Axios | RESTful API | ES6 | HTML5 | CSS3 | Google Analytics | Agile | Scrum | Flexbox | Kendo UI | TailwindCSS | Docker | Git | GitHub | Gitlab | Linux | MVC | Webpack| Bootstrap.</p>
                 </div> 
                 
                 <div className="service">
-                    <h3>React</h3>
+                    <h3>Full Stack Engineer</h3>
                     <p>I'm more of a UI focused developer. I'm familiar with React 
                       and Redux principles and patterns, I have experience in 
                       React building hierarchy stateful components for SPA's. I’m 
                       proficient in using the state management tool Redux to support 
-                      dispatching actions and manage state throughout the entire application.  
-                      I always like to keep in consistent communication with product teams, 
+                      dispatching actions and manage state throughout the entire application. 
+                      I'm also comfortable with utilizing Hooks and Context API to support maintaining 
+                      the state in React. I can also develop in the backend with NodeJS, ExpressJS and 
+                      MongoDB. I always like to keep in consistent communication with product teams, 
                       UX teams, team leads and peers working on the same or different projects.</p>
                 </div>
             </div> 
