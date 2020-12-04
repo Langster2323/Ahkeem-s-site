59cb3774362e7e4f02ee8a6c5d538928ae1a5ebd
Langster2323 <langahkeem@gmail.com>
2020-12-04 06:53:24 -0600

    removed unnecessary work

--- src/pages/home-page.js
+++ src/pages/home-page.js
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from 'react';
+import React from 'react';
 import '../App.css';
 
 import Header from '../components/header-component';
@@ -6,33 +6,6 @@ import Services from '../components/service-component';
 import AboutMe from '../components/home-components/aboutme-component';
 import MyWork from '../components/mywork-component';
 import Footer from '../components/footer-component';
-
-import Aos from 'aos';
-import "aos/dist/aos.css";
-import { Power3, gsap, } from 'gsap';
-
-import { useIntersection } from 'react-use';
-
-
-
-
-// const whatIDoArr = [
-//     {
-//         title: "Data Engineer",
-//         image: `${laptop}`,
-//         description: "At the moment I use PowerBI to visualize my data and I use the BI connector to connect PowerBI with my data source MongoDB Atlas. Ive used Azure Data Factory and ETL pipeline to ingest data into a Azure Data Lake gen two."
-//     },
-//     {
-//         title: "Skills",
-//         image: `${passion}`,
-//         description: "ReactJS | Hooks | Context API | Redux | NodeJS | ExpressJS | MongoDB | PowerBI"
-//     },
-//     {
-//         title: "Full Stack Engineer",
-//         image: `${laptop}`,
-//         description: "I'm more of a UI focused developer. I'm familiar with ReactJS, and tools within its ecosytem like Hooks, and Redux. I also love to work with animations to style up the frontend."
-//     }
-// ]
 
 function Home() {
 
